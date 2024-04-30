573f6d57e60422698cceb9c383fe653fff54629e
Lkomn <157511314+Lkomn@users.noreply.github.com>
2024-04-30 22:51:43 +0000

    k

---
+++ index.js
@@ -0,0 +1,71 @@
+require('dotenv').config();
+const express = require('express');
+const passport = require('passport');
+const Discord = require('passport-discord');
+const session = require('express-session');
+const memorystore = require('memorystore')(session);
+const url = require('node:url')
+
+const app = express();
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((id, done) => {
+    done(null, id)
+});
+
+passport.use(new Discord({
+    clientID: process.env.client_id,
+    clientSecret: process.env.client_secret,
+    callbackURL: process.env.redirect_uri,
+    scope: ["identify", "guilds"]
+}, (accessToken, refreshToken, profile, done) => {
+    process.nextTick(()=>done(null, profile));
+}));
+
+app.use(session({
+    store: new memorystore({ checkPeriod: 86400000 }),
+    secret: "%/%/%//%/%//%/%%%%==%//%",
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.engine('ejs', require('ejs').renderFile)
+app.set('view engine', './views/');
+
+app.get('/', (req, res) => {
+    console.log(req.isAuthenticated() ? req.user : null);
+    res.send('Hello!')
+});
+
+app.get('/api/login', (req, res, next) => {
+    if (req.session.backURL) {
+        req.session.backURL = req.session.backURL;
+    } else if (req.headers.referer) {
+        const parse = url.parse(req.headers.referer);
+        req.session.backURL = parse.path;
+    } else {
+        req.session.backURL = "/"
+    }
+
+    next();
+}, passport.authenticate('discord'));
+
+app.get('/api/callback', passport.authenticate('discord', { failureRedirect: "/" }), (req, res) => {
+    if (req.session.backURL) {
+        const backURL = req.session.backURL;
+        req.session.backURL = null;
+        res.redirect(backURL);
+    } else {
+        res.redirect('/')
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Alive.');
+})
