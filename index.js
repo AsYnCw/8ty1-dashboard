f70ab2e450bdab2e79f1d154b59bca626c1fc0c9
Lkomn <157511314+Lkomn@users.noreply.github.com>
2024-04-30 23:27:24 +0000

    -p

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(passport.session());
 app.engine('ejs', require('ejs').renderFile)
 app.set('views', path.join(__dirname, '/views/'));
 
-app.use('/', express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, "views")));
 
 app.get('/', (req, res) => {
     res.render('index.ejs', {
